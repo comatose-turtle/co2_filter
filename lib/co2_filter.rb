bc96f05f12022efeae4de5209abee20ccf591cba
Tommy Orr <torr@pivotal.io>
2016-02-01 18:03:33 -0800

    Add content-boosted collaborative filter, enable bypass of content-based filter (in averaging filter) if pre-provided

--- lib/co2_filter.rb
+++ lib/co2_filter.rb
@@ -1,7 +1,11 @@
 module Co2Filter
-  def self.filter(current_user: , other_users: , items: , user_profile: nil)
+  def self.filter(current_user: , other_users: , items: nil, user_profile: nil, content_based_results: nil)
+    raise ArgumentError.new("An 'items' or 'content_based_results' argument must be provided.") unless items || content_based_results
     collab = Collaborative.filter(current_user: current_user, other_users: other_users)
-    if user_profile.is_a? ContentBased::UserProfile
+    
+    if content_based_results && content_based_results.is_a?(Results)
+      content = content_based_results
+    elsif user_profile.is_a? ContentBased::UserProfile
       content = ContentBased.filter(user: user_profile, items: items)
     else
       content = ContentBased.filter(user: current_user, items: items)
@@ -12,6 +16,16 @@ module Co2Filter
     end
     Results.new(hybrid)
   end
+
+  def self.content_boosted_collaborative_filter(current_user:, other_users:, items:)
+    content_boosted_users = {}
+    other_users.each do |user_id, ratings|
+      content_boosted_users[user_id] = ratings.merge(ContentBased.filter(user: ratings, items: items))
+    end
+    results = Collaborative.filter(current_user: current_user, other_users: content_boosted_users)
+    Results.new(results)
+  end
+
   autoload :VERSION, 'co2_filter/version'
   autoload :Collaborative, 'co2_filter/collaborative'
   autoload :ContentBased,  'co2_filter/content_based'
--- spec/unit/co2_filter_spec.rb
+++ spec/unit/co2_filter_spec.rb
@@ -203,6 +203,20 @@ describe Co2Filter do
       expect(Co2Filter::ContentBased).not_to have_received(:ratings_to_profile)
     end
 
+    it 'should not call the content-based filter if content-based results are provided' do
+      allow(Co2Filter::ContentBased).to receive(:filter)
+      result = Co2Filter::ContentBased::Results.new({})
+      Co2Filter.filter(current_user: user1, other_users: other_users, content_based_results: result)
+      expect(Co2Filter::ContentBased).not_to have_received(:filter)
+    end
+
+    it 'should throw an error if content-based results and items are nil' do
+      allow(Co2Filter::Collaborative).to receive(:filter) {Co2Filter::Collaborative::Results.new({1 => -10, 2 => -1, 3 => 4})}
+      allow(Co2Filter::ContentBased).to receive(:filter) {Co2Filter::ContentBased::Results.new({1 => -2, 2 => -1, 3 => 1})}
+      expect{Co2Filter.filter(current_user: user1, other_users: other_users, content_based_results: nil, items: nil)}.to raise_error(ArgumentError)
+    end
+
+
     it 'returns a combination of both filtering techniques' do
       allow(Co2Filter::Collaborative).to receive(:filter) {Co2Filter::Collaborative::Results.new({1 => 1, 2 => 3, 3 => 4, 5 => 2, 9 => 1})}
       allow(Co2Filter::ContentBased).to receive(:filter) {Co2Filter::ContentBased::Results.new({1 => 2, 4 => 4, 6 => 2, 7 => 5, 10 => 4})}
@@ -246,5 +260,235 @@ describe Co2Filter do
       expect(result[1]).to be > result[2]
       expect(result[1]).to be < result[3]
     end
+  end
+
+  context '#filter' do
+    let(:user1) {
+      {
+        1 => 5.0,
+        2 => 2.6,
+        3 => 3.5,
+        4 => 0.0,
+        5 => 1.0
+      }
+    }
+    let(:other_users) {
+      {
+        100 => {
+          1 => 2.0,
+          3 => 3.0,
+          4 => 2.0,
+          6 => 5.0,
+          7 => 5.0,
+          8 => 2.0
+        },
+        101 => {
+          1 => 1.0,
+          2 => 4.0,
+          3 => 4.7,
+          4 => 2.2,
+          6 => 1.0,
+          7 => 5.0,
+          8 => 1.0
+        },
+        102 => {
+          2 => 0.0,
+          3 => 4.4,
+          4 => 3.3,
+          5 => 2.2,
+          7 => 5.0
+        },
+        103 => {
+          1 => 4.7,
+          2 => 2.0,
+          3 => 3.0,
+          5 => 0.0,
+          6 => 1.0,
+          7 => 5.0,
+          8 => 4.0
+        },
+        104 => {
+          1 => 4.0,
+          2 => 3.5,
+          3 => 0.0,
+          4 => 4.0,
+          5 => 1.0,
+          7 => 5.0,
+          8 => 2.2
+        },
+        105 => {
+          1 => 5.0,
+          2 => 2.6,
+          3 => 3.5,
+          4 => 0.0,
+          5 => 1.0,
+          7 => 1.0,
+          8 => 4.0,
+        },
+        106 => {
+          1 => 0.0,
+          2 => 2.4,
+          3 => 1.5,
+          4 => 5.0,
+          5 => 4.0,
+          7 => 4.5,
+          8 => 1.5
+        },
+        107 => {
+          1 => 5.0,
+          2 => 5.0,
+          3 => 5.0,
+          4 => 5.0,
+          5 => 5.0,
+          6 => 5.0,
+          7 => 5.0,
+          8 => 5.0,
+          9 => 5.0
+        }
+      }
+    }
+    let(:items) {
+      {
+        0 => {
+          2 => 1.0,
+          4 => 0.2,
+          5 => 0.9,
+          6 => 0.5,
+          9 => 1.0
+        },
+        1 => {
+          1 => 0.75,
+          2 => 0.3,
+          4 => 0.1,
+          5 => 0.2,
+          7 => 0.9
+        },
+        2 => {
+          1 => 0.6,
+          2 => 1.0,
+          3 => 0.9,
+          4 => 0.1,
+          5 => 0.05,
+          6 => 1.0,
+          11 => 1.0,
+          12 => 0.4
+        },
+        3 => {
+          5 => 0.4,
+          7 => 0.1,
+          8 => 0.8,
+          9 => 1.0,
+          10 => 0.7,
+          11 => 0.7
+        },
+        4 => {
+          1 => 0.65,
+          3 => 0.2,
+          5 => 0.5,
+          7 => 0.2,
+          9 => 0.4,
+          11 => 1.0
+        },
+        5 => {
+          1 => 1.0,
+          2 => 1.0,
+          3 => 1.0,
+          4 => 1.0,
+          5 => 1.0,
+          6 => 1.0,
+          7 => 1.0,
+          8 => 1.0,
+          9 => 1.0,
+          10 => 1.0,
+          11 => 1.0,
+          12 => 1.0,
+          13 => 1.0,
+          14 => 1.0
+        },
+        6 => {
+          11 => 1.0,
+          12 => 1.0,
+          13 => 1.0,
+          14 => 1.0
+        },
+        7 => {
+          15 => 1.0,
+          2 => 0.7,
+          5 => 0.6,
+          3 => 0.3
+        },
+        8 => {
+          1 => 1.0,
+          2 => 1.0,
+          3 => 0.8,
+          4 => 0.9
+        },
+        9 => {
+          2 => 0.9,
+          4 => 0.9,
+          6 => 0.8,
+          8 => 0.8,
+          10 => 0.9,
+          12 => 0.95,
+          14 => 0.8
+        }
+      }
+    }
+
+    it 'should call the collaborative filter' do
+      allow(Co2Filter::Collaborative).to receive(:filter) {Co2Filter::Collaborative::Results.new({})}
+      Co2Filter.content_boosted_collaborative_filter(current_user: user1, other_users: other_users, items: items)
+      expect(Co2Filter::Collaborative).to have_received(:filter)
+    end
+
+    it 'should call the content-based filter' do
+      allow(Co2Filter::ContentBased).to receive(:filter) {Co2Filter::ContentBased::Results.new({})}
+      Co2Filter.content_boosted_collaborative_filter(current_user: user1, other_users: other_users, items: items)
+      expect(Co2Filter::ContentBased).to have_received(:filter).exactly(other_users.length).times
+    end
+
+    # it 'returns a combination of both filtering techniques' do
+    #   allow(Co2Filter::Collaborative).to receive(:filter) {Co2Filter::Collaborative::Results.new({1 => 1, 2 => 3, 3 => 4, 5 => 2, 9 => 1})}
+    #   allow(Co2Filter::ContentBased).to receive(:filter) {Co2Filter::ContentBased::Results.new({1 => 2, 4 => 4, 6 => 2, 7 => 5, 10 => 4})}
+    #   result = Co2Filter.content_boosted_collaborative_filter(current_user: user1, other_users: other_users, items: items)
+    #   expect(result).to be_a(Co2Filter::Results)
+    #   expect(result.keys.sort).to eq([1, 2, 3, 4, 5, 6, 7, 9, 10])
+    # end
+
+    # it 'double recommendation is a strong recommendation in the result' do
+    #   allow(Co2Filter::Collaborative).to receive(:filter) {Co2Filter::Collaborative::Results.new({1 => 10, 2 => 1, 3 => -4})}
+    #   allow(Co2Filter::ContentBased).to receive(:filter) {Co2Filter::ContentBased::Results.new({1 => 2, 2 => 1, 3 => -1})}
+    #   result = Co2Filter.content_boosted_collaborative_filter(current_user: user1, other_users: other_users, items: items)
+    #   expect(result[1]).to be > 0.5
+    #   expect(result[1]).to be > result[2]
+    #   expect(result[1]).to be > result[3]
+    # end
+
+    # it 'double disrecommendation is a strong disrecommendation in the result' do
+    #   allow(Co2Filter::Collaborative).to receive(:filter) {Co2Filter::Collaborative::Results.new({1 => -10, 2 => -1, 3 => 4})}
+    #   allow(Co2Filter::ContentBased).to receive(:filter) {Co2Filter::ContentBased::Results.new({1 => -2, 2 => -1, 3 => 1})}
+    #   result = Co2Filter.content_boosted_collaborative_filter(current_user: user1, other_users: other_users, items: items)
+    #   expect(result[1]).to be < -0.5
+    #   expect(result[1]).to be < result[2]
+    #   expect(result[1]).to be < result[3]
+    # end
+
+    # it 'strong recommendation and weak disrecommendation is a weak recommendation in the result' do
+    #   allow(Co2Filter::Collaborative).to receive(:filter) {Co2Filter::Collaborative::Results.new({1 => 10, 2 => 10, 3 => -4})}
+    #   allow(Co2Filter::ContentBased).to receive(:filter) {Co2Filter::ContentBased::Results.new({1 => -1, 2 => 2, 3 => -1})}
+    #   result = Co2Filter.content_boosted_collaborative_filter(current_user: user1, other_users: other_users, items: items)
+    #   expect(result[1]).to be > 0
+    #   expect(result[1]).to be < result[2]
+    #   expect(result[1]).to be > result[3]
+    # end
+
+    # it 'strong disrecommendation and weak recommendation is a weak disrecommendation in the result' do
+    #   allow(Co2Filter::Collaborative).to receive(:filter) {Co2Filter::Collaborative::Results.new({1 => -10, 2 => -10, 3 => 4})}
+    #   allow(Co2Filter::ContentBased).to receive(:filter) {Co2Filter::ContentBased::Results.new({1 => 1, 2 => -2, 3 => 1})}
+    #   result = Co2Filter.content_boosted_collaborative_filter(current_user: user1, other_users: other_users, items: items)
+    #   expect(result[1]).to be < 0
+    #   expect(result[1]).to be > result[2]
+    #   expect(result[1]).to be < result[3]
+    # end
   end
 end
